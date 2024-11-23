487ddcfdf4d1ccc38ecb557ad65c197743f4c1fa
DangTruongSa <dasoaiwk@gmail.com>
2024-11-23 15:57:15 +0700

    ok

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private bool isCursorVisible = false, isPanelVisible = false; // Trạng thái mặc định: ẩn con trỏ
+    public GameObject panel;
+
+    void Start()
+    {
+        // // Khởi tạo con trỏ bị ẩn và khóa vào giữa màn hình
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        panel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Kiểm tra nếu nhấn phím Esc
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Chuyển đổi trạng thái con trỏ và panel
+            isCursorVisible = !isCursorVisible;
+            isPanelVisible = !isPanelVisible;
+
+            // Cập nhật trạng thái con trỏ
+            Cursor.visible = isCursorVisible;
+            // Cursor.lockState = !isPanelVisible ? CursorLockMode.None : CursorLockMode.Locked;
+
+
+            // Hiển thị hoặc ẩn panel
+            panel.SetActive(isPanelVisible);
+        }
+        panel.SetActive(isPanelVisible);
+        if (isPanelVisible)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+
+    }
+
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public void IsPanelActive()
+    {
+        isPanelVisible = !isPanelVisible;
+    }
+}
